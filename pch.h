2a51c031b1ff91d7831a5a907bbb7cb653a28428
Wilfred Odere <wizwill6@gmail.com>
2019-08-29 08:36:35 +0200

    All the necessary includes for the project.

---
+++ pch.h
@@ -0,0 +1,12 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_video.h>
+#include "Screen.h"
+
+const std::string ls{ "\n\t" }; // User-defined variable
+
+#endif // !PCH_H
