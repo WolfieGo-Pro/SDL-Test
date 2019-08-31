9e7a37aa21f01b4e626dac5ad0691bda4deef5f6
Wilfred Odere <wizwill6@gmail.com>
2019-08-31 12:52:03 +0200

    Declaration of Instance variables for the 'Screen class

---
+++ Screen.h
@@ -0,0 +1,41 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <iostream>
+#include <string>
+#include <SDL.h>
+
+namespace wilfred {
+
+	class Screen {
+
+	public:
+		
+		
+		int64_t colorz; // hex code for window color
+		
+		Screen();
+		//~Screen();
+
+		bool init();
+
+		void render(int64_t color);
+
+		bool process_events();
+
+		void clear();
+
+	private:
+		static const int screen_length = 600, screen_width = 800;
+
+		SDL_Window *m_window;
+		SDL_Renderer* m_renderer;
+		SDL_Texture* m_texturer;
+
+		Uint32* pixel_buffer = new Uint32[screen_length * screen_width]; // an array of memory for storing info of pixels for textures
+
+	};
+
+}
+
+#endif // !SCREEN_H
