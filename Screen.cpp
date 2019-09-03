f50bb335909ec8eff989677bb49bba85d1e04fb0
Wilfred Odere <wizwill6@gmail.com>
2019-09-03 11:09:24 +0200

    made changes to the 'process_event' method.

--- Screen.cpp
+++ Screen.cpp
@@ -22,57 +22,63 @@ namespace wilfred {
 
 		if (SDL_Init(SDL_INIT_VIDEO || SDL_INIT_AUDIO) < 0) { // returns an error value of -1
 
-			std::cout << ls << "Error occured. SDL can't initialize." << std::endl;
+			std::cout << ls << "Error occured. SDL can't initialize." << SDL_GetError() << std::endl;
 
 			//SDL_GetError(); //returns a string containing the last error
 
 			return false;
 		}
+		else {
 
-		std::cout << ls << "SDL initialized.." << std::endl;
+			std::cout << ls << "SDL initialized.." << std::endl;
 
-		m_window =
-			SDL_CreateWindow("SDL Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_length, SDL_WINDOW_OPENGL);
+			m_window =
+				SDL_CreateWindow("SDL Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_length, SDL_WINDOW_OPENGL);
 
-		if (m_window == NULL) {
+			if (m_window == NULL) {
 
-			std::cout << ls << "Could not create Window." << SDL_GetError() << std::endl;
+				std::cout << ls << "Could not create Window." << SDL_GetError() << std::endl;
 
-			return false;
+				return false;
+			}
+
+			m_renderer =
+				SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+			if (m_renderer == NULL) {
+
+				std::cout << "Could not render." << SDL_GetError() << std::endl;
+
+				return false;
+			}
+
+			m_texturer =
+				SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, screen_width, screen_length);
+
+			if (m_texturer == NULL) {
+
+				std::cout << "Could not create texture." << std::endl;
+
+				return false;
+			}
+
 		}
-
-		m_renderer =
-			SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-		if (m_renderer == NULL) {
-
-			std::cout << "Could not render." << SDL_GetError() << std::endl;
-
-			return false;
-		}
-
-		m_texturer =
-			SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, screen_width, screen_length);
-
-		if (m_texturer == NULL) {
-
-			std::cout << "Could not create texture." << std::endl;
-
-			return false;
-		}
+		
 
 	}
 
-	void Screen::render(int64_t colorz) {
+	void Screen::render(Uint32 &colorz) {
+
+		this->color = colorz;
 
 		//allows us to set a block of memory with a value
-		memset(pixel_buffer, 00, screen_width * screen_length * sizeof(Uint32));
+		memset(pixel_buffer, color, screen_width * screen_length * sizeof(Uint32));
 		
 	    //color = 0x5726AF00; // 0x-RED-GREEN-BLUE-ALPHA. Based from the pointer member "m_texturer"
 
 		for (int i = 0; i < screen_length * screen_width; i++) { // changing the color of the window
 
-			pixel_buffer[i] = colorz;
+			pixel_buffer[i] = color;
 
 			//color = 0xFFFF3300; // YELLOW
 			//color = 0x90BB00AA; // GREEN
@@ -96,24 +102,25 @@ namespace wilfred {
 
 			if (events.type == SDL_QUIT) {
 
-				return false;
+				return true;
 
 			}
 
 		}
 
-		return true;
+		return false;
 	}
 
 	void Screen::clear() {
 
-		delete[] pixel_buffer;
-
 		SDL_DestroyWindow(m_window);
 		SDL_DestroyRenderer(m_renderer);
 		SDL_DestroyTexture(m_texturer);
+
+		delete[] pixel_buffer;
 		
 		SDL_Quit();
+		SDL_QUIT;
 	}
 
 
