e52a7b7804f00b342f09c63666a8478ed7dae1b6
Wilfred Odere <wizwill6@gmail.com>
2019-09-23 01:35:14 +0200

    Made thorough changes to the screen class to implement easier usage

--- Screen.h
+++ Screen.h
@@ -1,38 +1,54 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#include <iostream>
-#include <string>
-#include <SDL.h>
-
-namespace wilfred {
+namespace sdl_wilfred {
 
 	class Screen {
 
 	public:
-		
-		
-		Uint32 color; // hex code for window color
+
+		static const int screen_length = 600, screen_width = 800; // Window dimensions
+
+		Uint32 color; // 4-byte unsigned char storage for screen and pixel color
+
+		struct PRESET_COLORS {
+
+			// Color format 0x-RR-GG-BB-AA
+
+			Uint32 black_pr = 00;
+			Uint64 pink_pr = 25525525500;
+			Uint32 yellow_pr = 0xFFFF3300;
+			Uint32 green_pr = 0x90BB00AA;
+
+		};
 		
 		Screen();
-		//~Screen();
 
-		bool init();
+		bool init(); // initializes systems & subsystems from SDL lib / needed to run this program
 
-		void render(Uint32 &color);
+		void set_color(int x, int y, Uint8 red, Uint8 green, Uint8 blue); // set position and color values of pixels
 
-		bool process_events();
+		void preset_color(int x, int y, Uint32& colorz); // set position and use preset color values of pixels
 
-		void clear();
+
+		void set_pixels(int x, int y, Uint8 r, Uint8 g, Uint8 b); // render/draw individual pixel on the screen
+
+		void preset_pixels(Uint8 r, Uint8 g, Uint8 b); // set color, but use preset position of individual pixel on the screen (in the middle)
+
+
+		void update(); // updates/refreshes texture info on the screen
+
+		bool process_events(); // continuously checks for an event (action) throughout the program once called
+
+		void clear(); // destroys all initialized systems & subsystems from SDL lib and exits the program
 
 	private:
-		static const int screen_length = 600, screen_width = 800;
 
 		SDL_Window *m_window;
 		SDL_Renderer* m_renderer;
 		SDL_Texture* m_texturer;
 
-		Uint32* pixel_buffer = new Uint32[screen_length * screen_width]; // an array of memory for storing info of pixels for textures
+		Uint32* pixel_buffer; // an array of memory for storing info of pixels for textures
 
 	};
 
