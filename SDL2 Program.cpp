2237d5d1cdfd6ec88d9b97d6f069abaddcf9a85f
Wilfred Odere <wizwill6@gmail.com>
2019-10-05 08:37:19 +0200

    updating the main.cpp file for more features of the app

--- SDL2 Program.cpp
+++ SDL2 Program.cpp
@@ -16,7 +16,7 @@
 using namespace sdl_wilfred;
 
 int main(int argc, char *argv[]) {
-	
+
 	Screen screen;
 
 	std::cout << ls << "Hey!" << std::endl;
@@ -34,12 +34,29 @@ int main(int argc, char *argv[]) {
 	while (true) {
 
 		// Trying to gradually animate color pixels using the 'sin()' function
+		// Gonna be putting the pixel animations in a struct
 		
-		Uint32 run_time{ SDL_GetTicks() }; // gets the number of milliseconds since the program started
+		Uint32 run_time = SDL_GetTicks(); // gets the number of milliseconds since the program started
 
-		double slow_anim{ sin(run_time) * 0.01 };
+		auto red_anim{
 
-		std::cout << ls << slow_anim << std::endl;
+			(unsigned char)((sin(run_time * 0.0001) * 254))
+
+		};
+		
+		auto green_anim{
+
+			(unsigned char)((cos(run_time * 0.0002) * 229))
+
+		};
+
+		auto blue_anim{
+			
+			(unsigned char)((sin(run_time * 0.0003) * 204))
+
+		};
+
+		//std::cout << ls << "Ranges of pixels (min to max and vice versa) are : " << blue_anim << std::endl;
 
 		// use 'set_color()' to set position and color values of pixels
 		// use 'preset_color()' to set position and use preset color values of pixels
@@ -48,7 +65,7 @@ int main(int argc, char *argv[]) {
 
 			for (int y = 0; y < Screen::screen_length; y++) {
 
-				screen.preset_color(x, y, preset.black_pr);
+				screen.set_color(x, y, red_anim, green_anim, blue_anim);
 
 			}
 		}
@@ -56,7 +73,7 @@ int main(int argc, char *argv[]) {
 		// use 'set_pixel()' to render/draw individual pixel on the screen
 		// use 'preset_pixels()' to set color, but use preset position of individual pixel on the screen (usually in the middle)
 
-		screen.preset_pixels(255, 0, 0);
+		screen.preset_pixels(60, 30, blue_anim);
 
 		// looping a pixel pattern
 
