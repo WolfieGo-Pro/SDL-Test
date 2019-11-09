75affb6d6b3eb4ce4832c6229d706267308b53ae
Wilfred Odere <wizwill6@gmail.com>
2019-11-09 07:02:56 +0100

    main file updated

--- SDL2 Program.cpp
+++ SDL2 Program.cpp
@@ -5,9 +5,13 @@
 
   - Creating an SDL Window
 
-  - Screen Class
+  - Screen Class (see Screen.h & Screen.cpp)
 
   - Bit-Shifting (see Bit-Shifting.cpp)
+
+  - Implementation of Pixel animation (see Pixel_color.h)
+
+  - Particle struct & Particle organizer (see Particle.h & Particle.cpp)
 
 */
 
@@ -19,11 +23,11 @@ int main(int argc, char *argv[]) {
 
 	Screen screen;
 
-	std::cout << ls << "Hey!" << std::endl;
+	std::cout << "Hey!" << std::endl;
 
 	if (screen.init() == false) {
 
-		std::cout << ls << "Error on Start-up" << std::endl;
+		std::cout << "Error on Start-up" << std::endl;
 
 		screen.clear();
 
@@ -34,69 +38,60 @@ int main(int argc, char *argv[]) {
 		// Color format 0x-RR-GG-BB-AA
 
 		Uint32 black_fixed = 00;
-		Uint64 pink_fixed = 25525525500;
+		Uint32 blue_fixed = 0x132397AA;
+		Uint32 pink_fixed = 2552552550;
 		Uint32 yellow_fixed = 0xFFFF3300;
 		Uint32 green_fixed = 0x90BB00AA;
-
+		Uint32 indigo_fixed = 0x16032240;
 	};
 	PRESET_COLORS preset; // instantiates a struct object that contains preset color values in hex
 
+	// instantiating objects from subclass of 'Pixel_Color' class
 	Red red;
 	Green green;
 	Blue blue;
 
-	while (true) {
-
-		Uint32 run_time = SDL_GetTicks();// gets the number of milliseconds since the program started
-
-		// Animating color pixels using the trigonometric functions: sin() and cos()
-		
-		/*
-		auto red_pixel{
-
-			screen.animate_pixels("red", run_time)
-
-		};
+	// instantiating an 'organizer class that creates an array of particle objects
+	Particle_Organizer particle_organizer;
 	
-		auto green_pixel{
+	srand((unsigned int)time(NULL)); // changes the sequence of the 'rand()' function (see Particle.cpp)
 
-			screen.animate_pixels("green", run_time)
-
-		};
-
-		auto blue_pixel{
-
-			screen.animate_pixels("blue", run_time)
-
-		};
-		*/
-
-		auto r = red.animate(run_time);
+	while (true) {
 		
-		auto g = green.animate(run_time);
-
-		auto b = blue.animate(run_time);
-		
-		//std::cout << ls << "Ranges of pixels (min to max and vice versa) are : " << red << std::endl;
-
-		// use 'set_color()' to set position and color values of pixels
-		// use 'preset_color()' to set position and use preset color values of pixels
-
 		for (int x = 0; x < Screen::screen_width; x++) {
 
 			for (int y = 0; y < Screen::screen_length; y++) {
 
-				screen.set_color(x, y, r, g, b);
-
+				screen.preset_color(x, y, preset.indigo_fixed);
+				//screen.set_color(x, y, 13, 23, 97);
 			}
+
+		}
+		
+		Uint32 run_time = SDL_GetTicks(); // gets the number of milliseconds since the program started
+
+		auto r = red.animate(run_time);
+
+		auto g = green.animate(run_time);
+
+		auto b = blue.animate(run_time);
+
+		const Particle* const ptr_particles = particle_organizer.get_particles();
+
+		for (int i = 0; i < Particle_Organizer::NUMBER_OF_PARTICLES; i++) {
+
+			Particle particle = ptr_particles[i];
+			
+			// to make sure the particles do not pass the screen's width & length
+
+			int position_x = (int)((particle.m_position_x) * (Screen::screen_width / 2)); 
+			int position_y = (int)((particle.m_position_y) * (Screen::screen_length / 2));
+
+			screen.set_pixels(position_x, position_y, r, g, b); // renders/draws individual pixels on the screen
+
 		}
 
-		// use 'set_pixel()' to render/draw individual pixel on the screen
-		// use 'preset_pixels()' to set color, but use preset position of individual pixel on the screen (usually in the middle)
-
-		//screen.preset_pixels(60, 30, blue);
-
-		// Render/Update screen texture (pixel info ) in window
+		// Render/Update screen texture (pixel info/particles) in window
 
 		screen.update();
 
@@ -117,3 +112,27 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 
 }
+
+
+
+/*
+		// Animating color pixels using the trigonometric functions: sin() and cos()
+		
+		auto red_animated{
+
+			screen.animate_pixels('r', run_time)
+
+		};
+	
+		auto green_animated{
+
+			screen.animate_pixels('g', run_time)
+
+		};
+
+		auto blue_animated{
+
+			screen.animate_pixels('b', run_time)
+
+		};
+		*/
